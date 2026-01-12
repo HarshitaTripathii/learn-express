d0c48721053a17bda5ab6de30070b9ce7dc828e7
HarshitaTripathii <harshitat216@gmail.com>
2026-01-12 20:45:14 +0530

    todo basic api done

--- index.js
+++ index.js
@@ -115,6 +115,129 @@ app.delete("/users/:id", (req, res) => {
   });
 });
 
+const todos = [
+  {
+    id: 1,
+    task: "dsa",
+    complete: true,
+  },
+  {
+    id: 2,
+    task: "development",
+    complete: false,
+  },
+];
+
+///todos -> GET-> getAllTodo
+app.get("/todos", (req, res) => {
+  return res.json({
+    success: true,
+    data: todos,
+  });
+});
+
+///todos -> POST-> createNewTodo
+app.post("/todos", (req, res) => {
+  const body = req.body;
+
+  //empty data check
+  if (!body || !body.task || !body.complete) {
+    return res.json({
+      success: false,
+      message: "provide complete info",
+    });
+  }
+
+  //validation check
+  if (typeof body.task === Number || typeof body.complete === String) {
+    return res.json({
+      success: false,
+      message: "Enter valid data",
+    });
+  }
+
+  const newTodo = {
+    id: todos.length + 1,
+    task: body.task,
+    complete: body.complete,
+  };
+  todos.push(newTodo);
+  return res.json({
+    success: true,
+    data: todos,
+  });
+});
+
+// /todos/:id -> GET-> getTodoById
+app.get("/todos/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const todo = todos.find((u) => u.id === id);
+  if (!todo)
+    return res.json({
+      success: false,
+      message: "todo does not exists",
+    });
+  return res.json({
+    success: true,
+    data: todo,
+  });
+});
+
+// /todos/:id -> PUT-> updateTodoById
+app.put("/todos/:id", (req, res) => {
+  //get the id from url
+  const id = Number(req.params.id);
+  //get the index using id
+  const ind = todos.findIndex((u) => u.id === id);
+  //get the todo on this index "ind"
+  const todo = todos.find((u) => u.id === id);
+  //destructuring req.body object
+  const { task, complete } = req.body;
+  // create updated todo
+  const updatedTodo = {
+    id: todo.id,
+    task: task || todo.task,
+    complete: complete || todo.complete,
+  };
+  // set this updated todo in orginal todos array using indexing
+  todos[ind] = updatedTodo;
+  return res.json({
+    success: true,
+    data: todos[ind],
+  });
+});
+
+// /todos/:id/toggle -> PUT-> toggleTodoById
+app.put("/todos/:id/toggle", (req, res) => {
+  //get the id from url
+  const id = Number(req.params.id);
+  //get the todo using this id
+  const todo = todos.find((t) => t.id === id);
+
+  console.log("Before Complete : ", todo.complete);
+  //update the todo with opposite of its orginal value using not
+  todo.complete = !todo.complete;
+  console.log("After Complete : ", todo.complete);
+
+  return res.json({
+    success: true,
+    data: todos[ind],
+    allTodos: todos,
+  });
+});
+
+// /todos/:id -> DELETE-> deleteTodoById
+app.delete("/todos/:id", (req, res) => {
+  const id = Number(req.params.id);
+  // const todo = todos.find((t) => t.id === id);
+  const ind = todos.findIndex((t) => t.id === id);
+  todos.splice(ind, 1);
+  return res.json({
+    success: true,
+    data: todos,
+  });
+});
+
 app.listen(8000, () => {
   console.log("Server is runnnig at http://localhost:8000");
 });
